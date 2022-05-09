a1ab54a263ce8c9f7cb015d453e5d71fc4f91c19
Johann Munoz <johannmunoz.dev@gmail.com>
2022-05-09 12:21:27 +0930

    Moved gateway

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -4,14 +4,13 @@ const { ApolloGateway } = require('@apollo/gateway');
 const gateway = new ApolloGateway({
   serviceList: [
     { name: 'accounts', url: 'http://localhost:4001/query' },
-    { name: 'products', url: 'http://localhost:4002/query' },
-    { name: 'reviews', url: 'http://localhost:4003/query' },
+    // { name: 'products', url: 'http://localhost:4002/query' },
+    // { name: 'reviews', url: 'http://localhost:4003/query' },
   ],
 });
 
 const server = new ApolloServer({
   gateway,
-
   subscriptions: false,
 });
 
