da53cfc01026505ce82e7cb6d7a9f321b6e7d818
Johann Munoz <johannmunoz.dev@gmail.com>
2022-05-13 15:20:13 +0930

    Added db schema def reviews

---
+++ server/cmd/reviews/ent/schema/product.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Product holds the schema definition for the Product entity.
+type Product struct {
+	ent.Schema
+}
+
+// Fields of the Product.
+func (Product) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New),
+	}
+}
+
+// Edges of the Product.
+func (Product) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("reviews", Review.Type),
+	}
+}
