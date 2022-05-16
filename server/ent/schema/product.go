208fcbbe28d8f0cc4328b83787ef40e516bd4a28
Johann Munoz <johannmunoz.dev@gmail.com>
2022-05-16 15:15:33 +0930

    Added shared ent implementation

---
+++ server/ent/schema/product.go
@@ -0,0 +1,34 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Product holds the schema definition for the Product entity.
+type Product struct {
+	ent.Schema
+}
+
+// Fields of the Product.
+func (Product) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New),
+		field.String("name"),
+		field.String("upc"),
+		field.Int("price").Default(0),
+	}
+}
+
+// Edges of the Product.
+func (Product) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("manufacturer", Manufacturer.Type).
+			Ref("products").
+			Unique(),
+		edge.To("reviews", Review.Type),
+	}
+}
